8c9d91088fcad4c11d86b4b4e4bf179ae3144538
Teoman Öncü <proreigh@gmail.com>
2023-05-06 00:40:08 +0300

    teoman
    zombi respawn olayı yapıldı fakat durdurulamıyor düzenlenmesi gerek

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-   
+    [SerializeField]
+    private GameObject followerPrefab;
+
+    [SerializeField]
+    private float followerInterval = 1;
+
+    private int x;
+
+    private bool isSpawning = true;
+
+    void Start()
+    {
+        x = 0;
+        StartCoroutine(spawnEnemy(followerInterval, followerPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(2);
+
+        if (isSpawning && x < 50) // respawn durumunu ve düþman sayýsýný kontrol edin
+        {
+            GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+            x++;
+
+            if (x == 50)
+            {
+                isSpawning = false;
+                Destroy(enemy);
+            }
+            else
+            {
+                StartCoroutine(spawnEnemy(interval, enemy));
+            }
+        }
+    }
 }
