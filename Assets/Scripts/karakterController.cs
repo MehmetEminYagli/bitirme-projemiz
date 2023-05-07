7f21b92d01782147e172279cbd7cfd231ae01c6c
Teoman Öncü <proreigh@gmail.com>
2023-05-07 11:47:48 +0300

    teoman
    Canbarı düzeltildi

--- Assets/Scripts/karakterController.cs
+++ Assets/Scripts/karakterController.cs
@@ -12,44 +12,6 @@ public class karakterController : MonoBehaviour
 
     private bool isWalking = false;
 
-
-    //karakter saðlýk
-    public int startingHealth = 100;        // Baþlangýç saðlýðý
-    public int currentHealth;               // Mevcut saðlýk
-    public Slider healthBar;                // Saðlýk çubuðu
-    private bool isDead;                    // Karakterin ölüp ölmediðini tutar
-
-    private void Start()
-    {
-        currentHealth = startingHealth;
-        healthBar.maxValue = startingHealth; // Saðlýk çubuðu için maksimum deðeri ayarla
-        healthBar.value = startingHealth; // Saðlýk çubuðunu baþlangýç saðlýðýna ayarla
-    }
-
-    public void TakeDamage(int damage)
-    {
-        if (isDead) return;
-
-        currentHealth -= damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth); // Saðlýk deðerini sýnýrlandýr
-
-        float healthPercentage = (float)currentHealth / (float)startingHealth;
-        healthBar.value = currentHealth; // Saðlýk çubuðunu güncelle
-        healthBar.transform.localScale = new Vector3(healthPercentage, 1, 1); // Saðlýk çubuðunun ölçeklendirilmiþ geniþliðini güncelle
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        isDead = true;
-        // Karakter ölünce yapýlacak iþlemler
-    }
-
-
     public void FixedUpdate()
     {
         // karakterin yönünü belirlemek için joystick'ten gelen yön vektörü
